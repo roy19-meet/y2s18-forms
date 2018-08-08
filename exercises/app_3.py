20dce8e5d4bfe1c520cded0357ed63be7dae01f2
idane19-meet <idane19@meet.mit.edu>
2018-08-08 16:31:40 +0300

    w

---
+++ exercises/app_3.py
@@ -0,0 +1,21 @@
+from databases import *
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html', students=query_all())
+
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+    return render_template('student.html', student=query_by_id(student_id))
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_student_route():
+    return render_template('add.html', 
+
+
+
+
+app.run(debug=True)
